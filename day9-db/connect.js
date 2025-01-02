e2438293aefd8a7b60f414e5c81162590623f8b7
arjunQ21 <arjunq21@gmail.com>
2025-01-02 14:27:34 +0545

    Day 9: Made model and inserted to database

--- day9-db/connect.js
+++ day9-db/connect.js
@@ -1,12 +1,16 @@
 import { config } from 'dotenv'
 import mongoose from 'mongoose'
 
+import {insertCar} from "./models/car.js"
+
 config();
 
-mongoose.connect(process.env.MONGODB_URL).then(() => {
+mongoose.connect(process.env.MONGODB_URL).then(async () => {
     console.log("Connected to MongoDB.")
+    var insertDetails = await insertCar({ name: "Roadster", price: 240000, makeYear: "2018", manufacturer: "Tesla" })
+    console.log({insertDetails})
 })
     .catch(function (err) {
-        console.error("Error connecting to MongoDB")
+        console.error("Error connecting to MongoDB", err)
     })
 
