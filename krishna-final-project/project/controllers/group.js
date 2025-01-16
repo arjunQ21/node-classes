cb75a970ed20d30926826883b6cad8172df1b543
KrishnaGrg1 <gkrishnabahadur618@gmail.com>
2025-01-16 22:25:17 +0545

    Group create,view all public group,edit group,view single group

--- krishna-final-project/project/controllers/group.js
+++ krishna-final-project/project/controllers/group.js
@@ -1,16 +1,17 @@
+
 import catchAsync from "../helpers/catchAsync.js";
 import Group from "../models/group.js";
-import GroupMember from "../models/groupMember.js";
+
 
 const CreateGroup=catchAsync(async(req,res)=>{
-    const { name,description,creatorID,isPrivate}=req.body;
+    const { name,description,isPrivate}=req.body;
     const existingGroup=await Group.findOne({name:req.body.name});
 
     if(existingGroup){
         throw new Error ("Group Name already exist")
     }
     const newGroup=await Group.create({
-        name,description,creatorID,isPrivate
+        name,description,creatorID:req.user._id,isPrivate
     });
 
     return res.json({
@@ -20,14 +21,47 @@ const CreateGroup=catchAsync(async(req,res)=>{
 })
 const ViewAllPublicGroup=catchAsync(async(req,res)=>{
     const group=await Group.find({isPrivate:false});
-
     return res.json({
        group 
     })
+})
 
+const ViewGroup=catchAsync(async(req,res)=>{
+    const groupId = req.params.groupId; 
+    const group=await Group.findOne({_id:groupId});
+   if(!group){
+    throw new Error ("Group not found")
+   }
+    return res.json({
+       group 
+    })
+})
+
+const EditGroup=catchAsync(async(req,res)=>{
+    const groupId = req.params.groupId; 
+    const existingGroup=await Group.findOne({_id:groupId});
+   if(!existingGroup){
+    throw new Error ("Group not found")
+   }
+
+   if(existingGroup.creatorID!==req.user._id){
+    throw new Error ("Only creator can edit the group");
+   }
+
+   existingGroup.name=req.body.name;
+   existingGroup.description=req.body.description;
+   existingGroup.isPrivate=req.body.isPrivate;
+
+   await existingGroup.save();
+    return res.json({
+       "message":"Successfully Group updated"
+    })
 })
 
 
-const GroupController={CreateGroup,ViewAllPublicGroup};
+
+
+
+const GroupController={CreateGroup,ViewAllPublicGroup,ViewGroup,EditGroup};
 
 export default GroupController;
--- krishna-final-project/project/routes/group.js
+++ krishna-final-project/project/routes/group.js
@@ -4,13 +4,19 @@ import validate from "../middlewares/valdation.js";
 import validationSchema from '../validations/group.js'
 import GroupController from "../controllers/group.js";
 import captureUserAuthToken from "../middlewares/captureUserauthToken.js";
+import requireLogin from "../middlewares/requireLogin.js";
 const groupRouter=Router();
 
 
+groupRouter.post('/',captureUserAuthToken,requireLogin,validate(validationSchema.addGroup),GroupController.CreateGroup);
 
-groupRouter.post('/',captureUserAuthToken,validate(validationSchema.addGroup),GroupController.CreateGroup);
+groupRouter.get('/',captureUserAuthToken,requireLogin,GroupController.ViewAllPublicGroup);
 
-groupRouter.get('/',captureUserAuthToken,GroupController.ViewAllPublicGroup);
+groupRouter.get('/:groupId',captureUserAuthToken,requireLogin,GroupController.ViewGroup);
+
+groupRouter.put('/:groupId',captureUserAuthToken,requireLogin,GroupController.EditGroup);
+
+
 
 
 
