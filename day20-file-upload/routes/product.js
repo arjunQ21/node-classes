8be49a41247ed10afc7a0b8478e379667b44823b
arjunQ21 <arjunq21@gmail.com>
2025-01-20 14:45:44 +0545

    Day 20: File upload validations

--- day20-file-upload/routes/product.js
+++ day20-file-upload/routes/product.js
@@ -6,8 +6,32 @@ import productValidation from "../validations/product.js"
 import productController from "../controllers/product.js";
 import multer from "multer";
 
-const upload = multer({dest: "images/products"})
+// const upload = multer({dest: "images/products"})
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'images/products')
+    },
+    filename: function (req, file, cb) {
+        const extension = file.mimetype.split("/")[1];
+        const newFileName = `${req.currentProduct._id}.${extension}`
+        cb(null, newFileName)
+    }
+})
 
+const upload = multer({
+    storage: storage,
+    fileFilter: function (req, file, cb) {
+        const fileType = file.mimetype.split("/")[0];
+        if (fileType == 'image') {
+            cb(null, true);
+        } else {
+            cb(null, false)
+        }
+    },
+    limits: {
+        fileSize: 1024 * 1024 * 3 // 1 MB
+    }
+})
 
 const productRouter = Router();
 
@@ -31,9 +55,9 @@ singleProductRouter.get("/", productController.getSingle)
 singleProductRouter.post("/setImage", upload.single('productImage'), productController.uploadImage)
 
 // edit single product
-singleProductRouter.put("/", validate(productValidation.addNew), requireLogin, productController.updateSingle )
+singleProductRouter.put("/", validate(productValidation.addNew), requireLogin, productController.updateSingle)
 // delete single product
-singleProductRouter.delete("/", requireLogin, productController.deleteSingle )
+singleProductRouter.delete("/", requireLogin, productController.deleteSingle)
 
 
 export default productRouter;
